d32e0f5b13f99f3502a3340e67255b64adf1bdfb
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-10-13 14:34:25 +0700

    Update

--- S3Bucket/RandomWord.js
+++ S3Bucket/RandomWord.js
@@ -1,51 +1,66 @@
 exports.handler = function(event, context) {
 
   var responseData = {};
+  responseData["Result"] = "E-R-R-O-R";
   var responseStatus = "FAILED";
-  console.log("REQUEST RECEIVED:\n" + JSON.stringify(event));
+  console.log("Request Received:\n" + JSON.stringify(event));
 
   if (event.RequestType == "Delete") {
     responseStatus = "SUCCESS";
-    //sendResponse(event, context, responseStatus);
     responseData["Reason"] = "CloudFormation Delete Request";
     sendResponse(event, context, responseStatus, responseData);
     return;
   }
 
+  getRandomWord(responseData);
+
+  responseStatus = "SUCCESS";
+  responseData["SystemInput"] = event.ResourceProperties.SystemInput;
+  responseData["UserInput"] = event.ResourceProperties.UserInput;
+  responseData["Reason"] = "Call to Generate Random Word";
+
+  setTimeout(function () { sendResponse(event, context, responseStatus, responseData) }, 4444);
+
+};
 
 
-  var http = require("http");
+
+function getRandomWord(responseData) {
+
+  var https = require("https");
+
   var options = {
-    host: "random-word-api.herokuapp.com",
-    port: 80,
+    hostname: "random-word-api.herokuapp.com",
+    port: 443,
     path: "/word?number=1",
     method: "GET"
   };
-  var req = http.request(options, function(res) {
-    console.log("STATUS: " + res.statusCode);
-    console.log("HEADERS: " + JSON.stringify(res.headers));
-    res.setEncoding("utf8");
-    res.on("data", function (chunk) {
-      console.log("BODY: " + chunk);
+
+  console.log("External API Call Attempt for Random Word:\n" + JSON.stringify(options));
+
+  var request = https.request(options, function(response) {
+    console.log("External API Call Attempt Status Code: " + response.statusCode);
+    console.log("External API Call Attempt Response Headers: " + JSON.stringify(response.headers));
+
+    var body = "";
+    response.on("data", function(chunk) {
+      body += chunk;
     });
+    response.on("end", function() {
+      console.log(body);
+      var resultRandomWord = body.substring( ( body.indexOf("\"") + 1 ), body.lastIndexOf("\"") );
+      responseData["Result"] = resultRandomWord;
+    });
+
   });
-  req.on("error", function(e) {
-    console.log("problem with request: " + e.message);
+
+  request.on("error", function(error) {
+    console.log("External API Call Error:\n" + error);
   });
-  req.end();
 
+  request.end();
 
-
-  responseStatus = "SUCCESS";
-
-  responseData["SystemInput"] = event.ResourceProperties.SystemInput;
-  responseData["UserInput"] = event.ResourceProperties.UserInput;
-  responseData["Reason"] = "Called to Generate Random Word";
-  responseData["Result"] = "Result Word";
-
-  sendResponse(event, context, responseStatus, responseData);
-
-};
+}
 
 
 
@@ -53,7 +68,7 @@ function sendResponse(event, context, responseStatus, responseData) {
 
   var responseBody = JSON.stringify({
     Status: responseStatus,
-    Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
+    Reason: "Refer the Reason's Details at CloudWatch Log Stream: " + context.logStreamName,
     PhysicalResourceId: context.logStreamName,
     StackId: event.StackId,
     RequestId: event.RequestId,
@@ -61,7 +76,7 @@ function sendResponse(event, context, responseStatus, responseData) {
     Data: responseData
   });
 
-  console.log("RESPONSE BODY:\n", responseBody);
+  console.log("Composed Response Body:\n", responseBody);
 
   var https = require("https");
   var url = require("url");
@@ -78,16 +93,16 @@ function sendResponse(event, context, responseStatus, responseData) {
     }
   };
 
-  console.log("SENDING RESPONSE...\n");
+  console.log("Sending Response . . .\n");
 
   var request = https.request(options, function(response) {
-    console.log("STATUS: " + response.statusCode);
-    console.log("HEADERS: " + JSON.stringify(response.headers));
+    console.log("Status Code: " + response.statusCode);
+    console.log("Headers: " + JSON.stringify(response.headers));
     context.done();
   });
 
   request.on("error", function(error) {
-    console.log("sendResponse Error:" + error);
+    console.log("sendResponse Error:\n" + error);
     context.done();
   });
 
