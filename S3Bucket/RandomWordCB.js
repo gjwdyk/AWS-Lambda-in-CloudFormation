742232ad48c3e5e4383007af79230ec6be9cb52d
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-10-14 11:54:33 +0700

    Update

--- S3Bucket/RandomWordCB.js
+++ S3Bucket/RandomWordCB.js
@@ -1,5 +1,4 @@
 exports.handler = function(event, context) {
-
   var responseData = {};
   responseData["Result"] = "E-R-R-O-R";
   var responseStatus = "FAILED";
@@ -13,50 +12,46 @@ exports.handler = function(event, context) {
   }
 
   var https = require("https");
-
   var options = {
     hostname: "random-word-api.herokuapp.com",
     port: 443,
     path: "/word?number=1",
     method: "GET"
   };
-
   console.log("External API Call Attempt for Random Word:\n" + JSON.stringify(options));
-
   var request = https.request(options, function(response) {
     console.log("External API Call Attempt Status Code: " + response.statusCode);
     console.log("External API Call Attempt Response Headers: " + JSON.stringify(response.headers));
-
-    var body = "";
-    response.on("data", function(chunk) {
-      body += chunk;
-    });
-    response.on("end", function() {
-      console.log(body);
-      var resultRandomWord = body.substring( ( body.indexOf("\"") + 1 ), body.lastIndexOf("\"") );
-      responseStatus = "SUCCESS";
-      responseData["SystemInput"] = event.ResourceProperties.SystemInput;
-      responseData["UserInput"] = event.ResourceProperties.UserInput;
-      responseData["Reason"] = "Call to Generate Random Word";
-      responseData["Result"] = resultRandomWord;
+    responseData["SystemInput"] = event.ResourceProperties.SystemInput;
+    responseData["UserInput"] = event.ResourceProperties.UserInput;
+    if (response.statusCode == "200") {
+      var body = "";
+      response.on("data", function(chunk) {
+        body += chunk;
+      });
+      response.on("end", function() {
+        console.log(body);
+        var resultRandomWord = body.substring( ( body.indexOf("\"") + 1 ), body.lastIndexOf("\"") );
+        responseStatus = "SUCCESS";
+        responseData["Reason"] = "Call to Generate Random Word";
+        responseData["Result"] = resultRandomWord;
+        sendResponse(event, context, responseStatus, responseData);
+      });
+    } else {
+      responseData["Reason"] = "External API Call Attempt Status Code: " + response.statusCode;
       sendResponse(event, context, responseStatus, responseData);
-    });
-
+    }
   });
-
   request.on("error", function(error) {
     console.log("External API Call Error:\n" + error);
     sendResponse(event, context, responseStatus, responseData);
   });
-
   request.end();
-
 };
 
 
 
 function sendResponse(event, context, responseStatus, responseData) {
-
   var responseBody = JSON.stringify({
     Status: responseStatus,
     Reason: "Refer the Reason's Details at CloudWatch Log Stream: " + context.logStreamName,
@@ -66,12 +61,10 @@ function sendResponse(event, context, responseStatus, responseData) {
     LogicalResourceId: event.LogicalResourceId,
     Data: responseData
   });
-
   console.log("Composed Response Body:\n", responseBody);
 
   var https = require("https");
   var url = require("url");
-
   var parsedUrl = url.parse(event.ResponseURL);
   var options = {
     hostname: parsedUrl.hostname,
@@ -85,19 +78,15 @@ function sendResponse(event, context, responseStatus, responseData) {
   };
 
   console.log("Sending Response . . .\n");
-
   var request = https.request(options, function(response) {
     console.log("Status Code: " + response.statusCode);
     console.log("Headers: " + JSON.stringify(response.headers));
     context.done();
   });
-
   request.on("error", function(error) {
     console.log("sendResponse Error:\n" + error);
     context.done();
   });
-
   request.write(responseBody);
   request.end();
-
 }
